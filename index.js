442f4df7cc157f5748a0c87edec0024296860a2e
Xavy San <francisco.sandoval23b@udgvirtual.udg.mx>
2024-06-28 18:54:58 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const axios = require('axios');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.get('/auth', (req, res) => {
+    const authURL = `https://auth.mercadolibre.com/authorization?response_type=code&client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}`;
+    res.redirect(authURL);
+});
+
+app.get('/callback', async (req, res) => {
+    const { code } = req.query;
+
+    try {
+        const response = await axios.post('https://api.mercadolibre.com/oauth/token', {
+            grant_type: 'authorization_code',
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+            code: code,
+            redirect_uri: process.env.REDIRECT_URI
+        });
+
+        const { access_token, refresh_token } = response.data;
+        // Guarda tokens en la base de datos
+        res.send('Authorization successful!');
+    } catch (error) {
+        res.status(500).send('Error during authorization');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
